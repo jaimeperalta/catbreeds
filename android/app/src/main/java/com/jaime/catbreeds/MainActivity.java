a30af00b554dc14370df7e722d72c3bac6d8e8d1
jaimeperalta <andrescharris4@gmail.com>
2025-09-20 15:12:44 -0500

    init

---
+++ android/app/src/main/java/com/jaime/catbreeds/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jaime.catbreeds;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,26 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.jaime.catbreeds',
   appName: 'catbreeds',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    },
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#83B7E6",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
+    }
+  }
 };
 
 export default config;
